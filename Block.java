7f3a955f976df4cd6c213712f8573ea1a58e76b4
Helen <hyperrhelen@users.noreply.github.com>
2014-10-21 21:35:35 -0700

    Block.java
    This is the list of variables inside of the block

---
+++ Block.java
@@ -0,0 +1,13 @@
+//Block.java
+//Helen Chac
+
+import java.util.ArrayList;
+import java.util.Stack;
+import java.util.ListIterator;
+
+public class Block{
+    ArrayList<String> var;
+    Block(){
+	var = new ArrayList<String>();
+    }
+}
